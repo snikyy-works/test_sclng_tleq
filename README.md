83ee418c6ef86cd35a522fe780c01e273cde064d
snikyy-works <snikyy.works@gmail.com>
2024-10-16 16:39:10 +0200

    final commit

--- README.md
+++ README.md
@@ -1,9 +1,39 @@
-# Canvas for Backend Technical Test at Scalingo
+# Canvas for Backend Technical Test at Scalingo - Software Engineer Golang
 
-## Instructions
+## Description
 
-* From this canvas, respond to the project which has been communicated to you by our team
-* Feel free to change everything
+This software application provides a JSON API capable of performing the following tasks:
+
+- Return data about the last 100 public GitHub repositories.
+- Limit results according to various parameters (in this case: language and owner).
+
+To achieve this, I used the GitHub API. Please refer to the "Requirements" section to set up a token for communicating with this API without rate limits.
+
+First, I make a GET request to the API to retrieve the last 100 public GitHub repositories. API_URL: https://api.github.com/repositories?per_page=100&page=1
+
+Then, I have to make a request to each languages_url provided by the repositories to retrieve the number of bytes used by each programming language. It is important to use goroutines to handle 100 requests concurrently, avoiding the inefficiency of making requests one by one.
+
+Once the repository data is in the correct format, we can apply filters. I implemented filtering by **language** and **owner**. The filters are applied based on the parameters received in the request. Additional filters can be easily added by expanding the filterByType function.
+
+Please refer to the "Test" section to know how to use my JSON API.
+
+## Requirements
+
+#### Setup GITHUB_TOKEN to access GithubAPI without rate limits
+
+- **Step 1** : generate a "Personal Access Tokens" on Github
+- **Step 2** : create a .env file in this repository root
+- **Step 3** : add the token to the .env file as follows: *GITHUB_TOKEN=token_value*
+
+## Upgrade dependencies in go mod file + vendoring
+
+```bash
+go get -u all
+go mod tidy
+go mod vendor
+```
+
+(PS : go version cannot be upgraded, please refer to this error : *github.com/Scalingo/sclng-backend-test-v1: cannot compile Go 1.23 code*)
 
 ## Execution
 
@@ -15,7 +45,32 @@ Application will be then running on port `5000`
 
 ## Test
 
-```
+#### Test server is running
+
+```bash
 $ curl localhost:5000/ping
-{ "status": "pong" }
+# { "status": "pong" }
+```
+
+#### Get last 100 public repositories from GithubAPI
+
+```bash
+$ curl localhost:5000/repos
+# please find the 10/16/24 4pm results in json/all_repos.json 
+```
+
+#### Get public repositories from GithubAPI containing a specific programming language
+
+```bash
+# curl localhost:5000/repos/lang/{lang}
+$ curl localhost:5000/repos/lang/Java
+# please find the 10/16/24 4pm results in json/lang.json
+```
+
+#### Get public repositories from GithubAPI owned by a specific person
+
+```bash
+# curl localhost:5000/repos/owner/{owner}
+$ curl localhost:5000/repos/owner/engineyard
+# please find the 10/16/24 4pm results in json/owner.json
 ```
--- main.go
+++ main.go
@@ -5,10 +5,48 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/Scalingo/go-handlers"
 	"github.com/Scalingo/go-utils/logger"
 )
+
+// RepositoryFromAPI struct according to the result of GithubAPI
+type RepositoryFromAPI struct {
+	FullName string `json:"full_name"`
+	Owner    struct {
+		Login string `json:"login"`
+	} `json:"owner"`
+	RepoName     string `json:"name"`
+	LanguagesURL string `json:"languages_url"`
+}
+
+// LanguageDetail holds the byte count for each programming language.
+type LanguageDetail struct {
+	Bytes int `json:"bytes"`
+}
+
+// Language is a map where the key is the language name and the value is LanguageDetail.
+type Language map[string]LanguageDetail
+
+// Repository struct with data formatted
+type Repository struct {
+	FullName  string   `json:"full_name"`
+	Owner     string   `json:"owner"`
+	RepoName  string   `json:"repository"`
+	Languages Language `json:"languages"`
+}
+
+// Struct for the final response to match the JSON format of the exercise
+type RepositoriesResponse struct {
+	Repositories []Repository `json:"repositories"`
+}
+
+// Github API URL for the 100 last public repositories
+const githubAPIURL = "https://api.github.com/repositories?per_page=100&page=1"
+
+// Github Token to avoid rate limits defined by default with GithubAPI
+var githubToken string
 
 func main() {
 	log := logger.Default()
@@ -19,12 +57,23 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Retrieve GITHUB_TOKEN var to authenticate to the Github API
+	githubToken = os.Getenv("GITHUB_TOKEN")
+	if githubToken == "" {
+		log.WithError(fmt.Errorf("GITHUB_TOKEN environment variable not set in .env file")).Error("GITHUB_TOKEN env var not set")
+		os.Exit(1)
+	}
+
 	log.Info("Initializing routes")
 	router := handlers.NewRouter(log)
 	router.HandleFunc("/ping", pongHandler)
 	// Initialize web server and configure the following routes:
-	// GET /repos
-	// GET /stats
+	// GET /repos - Get all repositories
+	router.HandleFunc("/repos", repositoriesHandler)
+	// GET /repos/lang/{lang} - Get all repositories containing {lang} as programming languages
+	router.HandleFunc("/repos/lang/{lang}", repositoriesHandler)
+	// GET /repos/owner/{owner} - Get all repositories owned by {owner}
+	router.HandleFunc("/repos/owner/{owner}", repositoriesHandler)
 
 	log = log.WithField("port", cfg.Port)
 	log.Info("Listening...")
@@ -45,4 +94,176 @@ func pongHandler(w http.ResponseWriter, r *http.Request, _ map[string]string) er
 		log.WithError(err).Error("Fail to encode JSON")
 	}
 	return nil
+}
+
+// repositoriesHandler handles request to GithubAPI and formats data for a specific request
+func repositoriesHandler(w http.ResponseWriter, r *http.Request, params map[string]string) error {
+
+	log := logger.Get(r.Context())
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	// Retrieve filter if exists
+	var filterType, filterValue string
+	for fType, fValue := range params {
+		filterType = fType
+		filterValue = fValue
+	}
+
+	// Create the GET Request to GithubAPI to retrieve repositories
+	req, err := http.NewRequest("GET", githubAPIURL, nil)
+	if err != nil {
+		log.WithError(err).Error("Fail to create GET Request to fetch repositories")
+	}
+	// Set Auth with the github token defined in .env file
+	req.Header.Set("Authorization", "token "+githubToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.WithError(err).Error("Fail to make GET Request to fetch repositories")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.WithError(err).Error("HTTP Status not ok after fetching repositories")
+	}
+
+	// Parse response
+	var repositories []RepositoryFromAPI
+	if err := json.NewDecoder(resp.Body).Decode(&repositories); err != nil {
+		log.Fatal("Error decoding response:", err)
+	}
+
+	// Setup to fetch languages data concurrently
+	var wg sync.WaitGroup
+	reposResponse := make([]Repository, 0, len(repositories))
+	resultsCh := make(chan Repository) // Channel to collect results
+	errCh := make(chan error)          // Channel to collect errors
+
+	for _, repo := range repositories {
+		wg.Add(1)
+		go func(repo RepositoryFromAPI) {
+			defer wg.Done()
+			languages := make(map[string]int) // Temporary map to hold raw languages data
+			languagesURL := repo.LanguagesURL
+
+			// Create the GET Request to GithubAPI to retrieve languages data
+			req, err := http.NewRequest("GET", languagesURL, nil)
+			if err != nil {
+				errCh <- fmt.Errorf("failed to create request for languages for %s: %w", repo.FullName, err)
+				return
+			}
+
+			// Set Auth with the github token defined in .env file
+			req.Header.Set("Authorization", "token "+githubToken)
+
+			resp, err := client.Do(req)
+			if err != nil {
+				errCh <- fmt.Errorf("error fetching languages for %s: %w", repo.FullName, err)
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode != http.StatusOK {
+				errCh <- fmt.Errorf("error fetching languages for %s: %w", repo.FullName, err)
+				return
+			}
+
+			if err := json.NewDecoder(resp.Body).Decode(&languages); err != nil {
+				errCh <- fmt.Errorf("error decoding languages for %s: %w", repo.FullName, err)
+				return
+			}
+
+			// Convert the raw map to the desired structure
+			languagesDetail := make(Language) // This will hold the final structure
+			for lang, bytes := range languages {
+				languagesDetail[lang] = LanguageDetail{Bytes: bytes} // Wrap byte count in LanguageDetail
+			}
+
+			// Create the repository object
+			repoResponse := Repository{
+				FullName:  repo.FullName,
+				Owner:     repo.Owner.Login,
+				RepoName:  repo.RepoName,
+				Languages: languagesDetail,
+			}
+
+			// Send the result to the channel
+			resultsCh <- repoResponse
+		}(repo)
+	}
+
+	// Wait for all goroutines to finish
+	go func() {
+		wg.Wait()
+		close(resultsCh)
+		close(errCh)
+	}()
+
+	// Collect results and handle errors
+	for {
+		select {
+		case repo, ok := <-resultsCh:
+			if !ok {
+				// If results channel is closed, set it to nil
+				resultsCh = nil
+			} else {
+				reposResponse = append(reposResponse, repo)
+			}
+		case err, ok := <-errCh:
+			if !ok {
+				// If error channel is closed, set it to nil
+				errCh = nil
+			} else {
+				log.WithError(err).Error("Error occurred during language fetching")
+				return err // Return the first error encountered
+			}
+		}
+		// Exit the loop when both channels are closed
+		if resultsCh == nil && errCh == nil {
+			break
+		}
+	}
+
+	// If the request has filter, apply it
+	if filterType != "" {
+		// If a filter was provided, filter the results
+		reposResponse = filterByType(filterType, filterValue, reposResponse)
+	}
+
+	// Create final response format to match the JSON format of the exercise
+	response := RepositoriesResponse{
+		Repositories: reposResponse,
+	}
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		log.WithError(err).Error("Fail to encode JSON")
+	}
+
+	return nil
+}
+
+// filterByType filters repositories by language or owner
+// You can add more filter types here by adding new case in switch statements
+func filterByType(filterType, filterValue string, repos []Repository) []Repository {
+	var filteredRepos []Repository
+	switch filterType {
+	case "lang":
+		for _, repo := range repos {
+			if _, exists := repo.Languages[filterValue]; exists {
+				filteredRepos = append(filteredRepos, repo)
+			}
+		}
+	case "owner":
+		for _, repo := range repos {
+			if repo.Owner == filterValue {
+				filteredRepos = append(filteredRepos, repo)
+			}
+		}
+	default:
+		return repos
+	}
+	return filteredRepos
 }
